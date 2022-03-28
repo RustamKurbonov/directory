277dc868319b5d5e19423f4afaf4e03eaaf34ced
Rustam <kurbonovrustam47@gmail.com>
2022-03-28 18:08:12 +0300

    Add files via upload

---
+++ src/components/Header/HeaderContainer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from './Header'
+
+const HeaderContainer = props => {
+   return (
+      <Header loginUser = {props.loginUser} outLogin = {props.outLogin} />
+   )
+}
+
+export default HeaderContainer;
